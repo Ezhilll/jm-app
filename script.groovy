08d549609bb01d52c52e1bd4bb4ac49553e57b40
poornambiga <83727595+poornambiga@users.noreply.github.com>
2021-07-09 01:52:38 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,12 +5,12 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: '6ea27612-6a27-4815-90da-8cf2055a12bf', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t poornambiga/javaapp:jma1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push poornambiga/javaapp:jma1.0'
     }
-} 
+} :jma1.0
 
 def deployApp() {
     echo 'deploying the application...'
