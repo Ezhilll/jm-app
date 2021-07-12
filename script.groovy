f4a970312532c2813d0f0454bcd20d7416e38f0e
Ezhilll <86719592+Ezhilll@users.noreply.github.com>
2021-07-12 16:46:41 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: '6ea27612-6a27-4815-90da-8cf2055a12bf', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'DOCKERCREDENTIALS', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t poornambiga/javaapp:jma1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push poornambiga/javaapp:jma1.0'
